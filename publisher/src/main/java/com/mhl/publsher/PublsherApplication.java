c17a0ab9d8caedccc6ae6100ba1c07eb1d87f2e2
Grey-Mh <m1138453218@163.com>
2024-08-15 20:07:59 +0800

    第一次提交

---
+++ publisher/src/main/java/com/mhl/publsher/PublsherApplication.java
@@ -0,0 +1,13 @@
+package com.mhl.publsher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublsherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublsherApplication.class, args);
+    }
+
+}
---
+++ publisher/src/test/java/com/mhl/publsher/PublsherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhl.publsher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublsherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
